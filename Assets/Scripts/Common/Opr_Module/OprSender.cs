e6ce58f69d09c0d29bf2c16a320ab501aa240443
zhangjunchi <zhangjunchi@conchship.net>
2023-11-29 13:31:45 +0800

    0

--- Assets/Scripts/Common/Opr_Module/OprSender.cs
+++ Assets/Scripts/Common/Opr_Module/OprSender.cs
@@ -1,12 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace Common.Opr_Module
 {
     public class OprSender : MonoBehaviour, IPointerClickHandler
     {
-        public delegate void Click_Handle(PointerEventData eventData);
-        public event Click_Handle click_event;
+        public event Action<PointerEventData> click_event;
 
         //==================================================================================================
 
