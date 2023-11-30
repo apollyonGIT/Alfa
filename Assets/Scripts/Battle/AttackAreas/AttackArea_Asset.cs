78bde6a39b61e92e2028d6c52a74e1a49a5cb271
zhangjunchi <zhangjunchi@conchship.net>
2023-11-30 18:30:34 +0800

    0

--- Assets/Scripts/Battle/AttackAreas/AttackArea_Asset.cs
+++ Assets/Scripts/Battle/AttackAreas/AttackArea_Asset.cs
@@ -1,14 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Battle.AttackAreas
 {
-    [CreateAssetMenu(fileName = "attack_area_asset", menuName = "attack_area")]
+    [CreateAssetMenu(fileName = "attack_area_asset", menuName = "DIY_Asset/attack_area")]
     public class AttackArea_Asset : ScriptableObject
     {
-        //[System.Serializable]
-
+        public (int, int) pos_array;
     }
 }
 
