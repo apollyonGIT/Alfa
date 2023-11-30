ab4443c90620193b15d9b38f08b8f4ccab1b2644
zhangjunchi <zhangjunchi@conchship.net>
2023-11-30 18:24:33 +0800

    --Add 1. 静态数据。

---
+++ Assets/Scripts/Battle/AttackAreas/AttackArea_Asset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Battle.AttackAreas
+{
+    [CreateAssetMenu(fileName = "attack_area_asset", menuName = "attack_area")]
+    public class AttackArea_Asset : ScriptableObject
+    {
+        //[System.Serializable]
+
+    }
+}
+
+
+
+
