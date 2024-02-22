1174c2ade90c1871af4488266f575f98023e41b2
zhangjunchi <zhangjunchi@conchship.net>
2024-02-22 18:24:04 +0800

    1

--- Assets/Scripts/Battle/BattleSceneInput.cs
+++ Assets/Scripts/Battle/BattleSceneInput.cs
@@ -15,18 +15,17 @@ namespace Battle
         {
             Battle_Mouse_Helper.instance.calc_mouse_pos(out var pos);
 
-            var hit = Physics2D.Raycast(pos, Vector2.zero).transform;
-            if (hit == null)
-            {
-                Mission.instance.try_get_mgr("InteractiveMgr", out var interactive_mgr);
-                interactive_mgr.GetType().GetMethod("notify_on_left_click_null")?.Invoke(interactive_mgr, null);
-                return;
-            }
-
-            if (!hit.TryGetComponent(out InputView view)) return;
-
-            var mgr = view.iv_mgr;
-            mgr.GetType().GetMethod("notify_on_left_click")?.Invoke(mgr, new object[] { view.iv_cell });
+            EX_Utility.raycast(pos,
+                () =>
+                {
+                    Mission.instance.try_get_mgr("InteractiveMgr", out var interactive_mgr);
+                    interactive_mgr.GetType().GetMethod("notify_on_left_click_null")?.Invoke(interactive_mgr, null);
+                },
+                (view) =>
+                {
+                    var mgr = view.iv_mgr;
+                    mgr.GetType().GetMethod("notify_on_left_click")?.Invoke(mgr, new object[] { view.iv_cell });
+                });
         }
 
 
