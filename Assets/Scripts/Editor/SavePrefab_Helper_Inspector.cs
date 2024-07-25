329a35bf616985354ea0900fe98908971a027f83
zhangjunchi <zhangjunchi@conchship.net>
2024-07-25 11:57:04 +0800

    --Upd 1. SavePrefab_Helper面板增强。

---
+++ Assets/Scripts/Editor/SavePrefab_Helper_Inspector.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editors.Inspectors
+{
+    [CustomEditor(typeof(SavePrefab_Helper))]
+    public class SavePrefab_Helper_Inspector : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("do"))
+            {
+                var instance = (SavePrefab_Helper)target;
+                instance.@do();
+            }
+        }
+    }
+}
+
