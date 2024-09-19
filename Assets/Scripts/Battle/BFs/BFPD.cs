4fe2c12d8f8e3a5d8c16b50eac836f4c06445b88
zhangjunchi <zhangjunchi@conchship.net>
2024-09-19 14:17:25 +0800

    --Add 1. 战场区。

---
+++ Assets/Scripts/Battle/BFs/BFPD.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+using Foundation;
+
+namespace Battle.BFs
+{
+    public class BFPD : Producer
+    {
+        public BFView model;
+
+        public override IMgr imgr => null;
+
+        //==================================================================================================
+
+        public override void init(int priority)
+        {
+            Instantiate(model, transform);
+        }
+
+
+        public override void call()
+        {
+        }
+    }
+}
+
