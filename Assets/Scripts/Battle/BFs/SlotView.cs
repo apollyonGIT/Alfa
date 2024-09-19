55a47d5119c5cf39072f9aef49f0867a3e59e6f9
zhangjunchi <zhangjunchi@conchship.net>
2024-09-19 15:07:16 +0800

    --Add 1. slot交互。

---
+++ Assets/Scripts/Battle/BFs/SlotView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Battle.BFs
+{
+    public class SlotView : MonoBehaviour
+    {
+
+        //==================================================================================================
+
+        public void notify_on_left_click()
+        {
+            Debug.Log(111);
+        }
+    }
+}
+
